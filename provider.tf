52d2d0b4edd71b902e4aaba14a85403dc200a75a
Mordecai Obase <omordecai2@gmail.com>
2024-07-06 15:13:16 -0500

    Added SAST block

---
+++ provider.tf
@@ -0,0 +1,33 @@
+terraform {
+  # required_version = ">= 1.3.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.55.0"
+    }
+
+  }
+}
+
+
+
+provider "aws" {
+  #region = "us-east-1"
+  #profile = "AWS-CLI-User"
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "gitlab-pipeline-2021"
+    key            = "jjtech/terraform.tfstate"
+    region         = "us-east-1"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "gitlab-pipeline-table"
+  }
+ }
+
+
+
+
